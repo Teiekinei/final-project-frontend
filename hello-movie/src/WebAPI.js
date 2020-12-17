f6c2f05b34a0d0626c4ae3bbe29973317430dd49
miluku1018 <miluku1018@gmail.com>
2020-12-17 21:27:22 +0800

    upadte WebAPI

--- hello-movie/src/WebAPI.js
+++ hello-movie/src/WebAPI.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://movie-api.jas0nhuang.tw";
+const BASE_URL = "https://movie-api.jas0nhuang.tw";
 
 export const getLatestMovies = () => {
   return fetch(`${BASE_URL}/movies-thisweek`).then((res) => res.json());
